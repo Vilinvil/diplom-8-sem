926d7c0fba4d4a950bab10162325ea19c312f17e
Vilinvil <ivn15072002@gmail.com>
2024-04-18 15:59:00 +0300

    add figure_lines_by_two_points.m

---
+++ figure/figure_lines_by_two_points.m
@@ -0,0 +1,6 @@
+function figure_lines_by_two_points()
+    for k = 1:length(lines)
+        xy = [lines(k).point1; lines(k).point2];
+        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+    end
+end
